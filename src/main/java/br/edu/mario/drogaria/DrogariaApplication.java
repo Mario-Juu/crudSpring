0bd80ef63876444bb0e05c1d4ea87e3303f2f673
Mario Júnior <mariojralves2006@gmail.com>
2023-11-07 12:08:31 -0300

    first commit

---
+++ src/main/java/br/edu/mario/drogaria/DrogariaApplication.java
@@ -0,0 +1,14 @@
+package br.edu.mario.drogaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrogariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrogariaApplication.class, args);
+
+	}
+
+}
